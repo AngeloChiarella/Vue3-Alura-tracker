f4197479eac7ce6b9e0159240c909a90c067eb53
AngeloChiarella <chiarella.gaetano@gmail.com>
2022-06-24 10:07:06 -0300

    Vue3: explorando o framework

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,59 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="" />
+    </h1>
+    <button v-if="modoEscuroAtivo" class="button is-warning" @click="alterarTema">
+      {{ textoDoBotao }}
+    </button>
+    <button v-if="!modoEscuroAtivo" class="button is-dark" @click="alterarTema">
+      {{ textoDoBotao }}
+    </button>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BarraLateral",
+  emits: ["aoTemaAlterado"],
+  data() {
+    return {
+      modoEscuroAtivo: false,
+    };
+  },
+  computed: {
+    textoDoBotao() {
+      if (this.modoEscuroAtivo) {
+        return "Tema claro";
+      }
+      return "Tema escuro";
+    },
+  },
+  methods: {
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo;
+      this.$emit("aoTemaAlterado", this.modoEscuroAtivo);
+    },
+  },
+});
+</script>
+
+<style scoped>
+/* Todo o estilo do componente */
+
+header {
+  padding: 1rem;
+  background: var(--bg-barra-primario);
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box has-text-weight-bold" :style="estilos">
+    <slot> </slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BoxAlura",
+  data() {
+    return {
+      estilos: {
+        color: "var(--texto-primario)",
+        background: "var(--bg-box-primario)",
+      },
+    };
+  },
+});
+</script>
